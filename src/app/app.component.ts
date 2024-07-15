5976d24b643d330c6dc14b9cbfe5cef6ebd3570b
RSamaium <sam@newbot.io>
2024-07-15 10:39:31 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    template: '<h1>Mon App</h1>',
+    standalone: true
 })
-export class AppComponent {
-  title = 'my-app';
-}
+export class AppComponent {}
